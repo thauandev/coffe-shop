e437a18ed8bcad6f2019871adece93f09332c15f
Thauan de Paula <thpc@Thauans-MacBook-Pro.local>
2023-09-05 15:03:29 -0300

    feature: start project with header

---
+++ src/app/components/Header/index.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { MapPin } from "@phosphor-icons/react";
+import Image from "next/image";
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <header className="flex justify-between container mx-auto">
+      <Image
+        loading="eager"
+        width={85}
+        height={40}
+        src="/logo.png"
+        alt="logo"
+      />
+      <div className="flex">
+        <div>
+          <MapPin color="var(--purple)" size={32} weight="fill" />
+        </div>
+        <div>
+          <span className="text-purple-200">Porto Alegre, RS</span>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
